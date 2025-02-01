44450343fbef4c7c8eff93d12e104b9b45142f74
Yaswanthv5 <89481859+Yaswanthv5@users.noreply.github.com>
2025-02-02 01:05:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,16 +6,16 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
   title: {
-    default: "chronark.com",
-    template: "%s | chronark.com",
+    default: "Yaswanthreddyvarikuti",
+    template: "%s | Yaswanthreddy.com",
   },
-  description: "Co-founder of unkey.dev and founder of planetfall.io",
+  description: "Data Engineer and AI Engineer",
   openGraph: {
-    title: "chronark.com",
+    title: "Yaswanthreddy.com",
     description:
-      "Co-founder of unkey.dev and founder of planetfall.io",
-    url: "https://chronark.com",
-    siteName: "chronark.com",
+      "Data Engineer and AI Engineer",
+    url: "https://Yaswanthreddy.com",
+    siteName: "Yaswanthreddy.com",
     images: [
       {
         url: "https://chronark.com/og.png",
@@ -38,7 +38,7 @@ export const metadata: Metadata = {
     },
   },
   twitter: {
-    title: "Chronark",
+    title: "Yaswanthreddy",
     card: "summary_large_image",
   },
   icons: {
