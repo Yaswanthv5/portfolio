0473583b21e2c04058e55798005a8a3653b6873d
Yaswanth <yyaswanthreddy5@gmail.com>
2025-02-03 00:04:17 +0530

    update resume

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -2,13 +2,14 @@
 
 import React from "react";
 import { FaDownload } from "react-icons/fa";
+import { Navigation } from "../components/nav";
 
 export default function ResumeDownload() {
     const handleDownload = () => {
         const resumePath = "/yaswanthreddyvarikuti.pdf"; // Update this with the actual resume file path
         const link = document.createElement("a");
         link.href = resumePath;
-        link.download = "YaswanthReddy_Resume.pdf";
+        link.download = "YaswanthReddyvarikuti.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
@@ -16,9 +17,10 @@ export default function ResumeDownload() {
 
     return (
         <div className="flex justify-center mt-8">
+            <Navigation />
             <button
                 onClick={handleDownload}
-                className="flex items-center gap-2 px-6 py-3 text-lg font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
+                className="flex items-center gap-2 px-6 py-8 text-lg font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
             >
                 <FaDownload />
                 Resume
