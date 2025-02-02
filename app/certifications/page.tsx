17aee5fdf531722b458ae101986a10105befe49b
Yaswanth <yyaswanthreddy5@gmail.com>
2025-02-02 23:55:51 +0530

    update certifications page

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 // import { Github, Mail, Linkedin } from "lucide-react";
 // import Link from "next/link";
+import Image from "next/image";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
@@ -24,28 +25,27 @@ const certificates = [
 
 export default function Example() {
     return (
-        <div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
+        <div className=" flex flex-col items-center py-10 bg-zinc-900 text-white">
             <Navigation />
             <h1 className="text-4xl font-bold mb-8 mt-16 text-center text-white-400">Certifications</h1>
             <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
                 <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-                    {certificates.map((s) => (
+                    {certificates.map((s, index) => (
                         <Card>
-                                <span
-                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                                    aria-hidden="true"
+                            <div key={index} className="flex flex-col items-center justify-center p-6 space-y-4 text-center">
+                                <Image  
+                                src={s.icon} 
+                                alt={`${s.label} Certification`} 
+                                width={200} height={100} 
+                                className="rounded-lg shadow-md" 
                                 />
-                                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                                    {s.icon}
-                                </span>{" "}
-                                <div className="z-10 flex flex-col items-center">
-                                    <span className="lg:text-xl font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
-                                        {s.handle}
-                                    </span>
-                                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                                        {s.label}
-                                    </span>
-                                </div>
+                                <span className="lg:text-xl font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                    {s.handle}
+                                </span>
+                                <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                    {s.label}
+                                 </span>
+                             </div>
                         </Card>
                     ))}
                 </div>
