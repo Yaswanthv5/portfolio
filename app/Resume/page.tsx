3384fee44545caa5eda8ac5a4f3d9957f09f95d5
Yaswanth <yyaswanthreddy5@gmail.com>
2025-02-03 00:11:52 +0530

    update resume page

--- app/Resume/page.tsx
+++ app/Resume/page.tsx
@@ -6,21 +6,21 @@ import { Navigation } from "../components/nav";
 
 export default function ResumeDownload() {
     const handleDownload = () => {
-        const resumePath = "/yaswanthreddyvarikuti.pdf"; // Update this with the actual resume file path
+        const resumePath = "public/yaswanthreddyvarikuti.pdf"; // Update this with the actual resume file path
         const link = document.createElement("a");
         link.href = resumePath;
-        link.download = "YaswanthReddyvarikuti.pdf";
+        link.download = "yaswanthreddyvarikuti.pdf";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
     };
 
     return (
-        <div className="flex justify-center mt-8">
+        <div className="flex flex-col items-center mt-8">
             <Navigation />
             <button
                 onClick={handleDownload}
-                className="flex items-center gap-2 px-6 py-8 text-lg font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
+                className="flex items-center gap-2 px-6 py-3 mt-12 text-lg font-semibold text-white bg-blue-600 rounded-lg shadow-md hover:bg-blue-700 transition-all duration-300"
             >
                 <FaDownload />
                 Resume
