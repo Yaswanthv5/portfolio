98b1e6e27b96d2ec9ece0c8a7231e3fb0c5895b1
Yaswanthv5 <89481859+Yaswanthv5@users.noreply.github.com>
2025-02-02 12:08:16 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -35,14 +35,26 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+           As a highly motivated and results - orientated AI and
+           Data Expert with two years of hands-on experience, I
+            possess a strong foundation in automating data
+            pipelines and streamlining routine workloads, enabling
+            organizations to maximize efficiency and unlock
+            valuable insights. I am proficient in cutting-edge
+            technologies, including generative AI, machine
+            learning, and deep learning, leveraging these to
+            develop innovative and impactful solutions. My
+            expertise extends to cloud platforms such as
+            Snowflake, AWS, and GCP, allowing me to design and
+            implement robust and scalable data architectures. I am
+            adept at data engineering principles, ensuring data
+            quality and integrity throughout the entire data
+            lifecycle. Furthermore, I possess a strong
+            understanding of data science methodologies, enabling
+            me to extract meaningful insights from complex
+            datasets and build predictive models that drive
+            informed decision-making. I am committed to
+            continuous learning. 
         </h2>
       </div>
     </div>
