94011f5478a1104d620cb06c600ad80bcc8e0b4c
Yaswanth <yyaswanthreddy5@gmail.com>
2025-02-23 23:11:24 +0530

    updating certs page

--- app/certifications/page.tsx
+++ app/certifications/page.tsx
@@ -101,47 +101,49 @@ export default function Example() {
             <div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
                 <div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
                     {certificates.map((s, index) => (
-                        <><Card>
+                        <Card>
                             <div key={index} className="flex flex-col items-center justify-center p-6 space-y-4 text-center">
-                                <Image
-                                    src={s.icon}
-                                    alt={`${s.label} Certification`}
-                                    width={200} height={100}
-                                    className="rounded-lg shadow-md" />
+                                <Image  
+                                src={s.icon} 
+                                alt={`${s.label} Certification`} 
+                                width={200} height={100} 
+                                className="rounded-lg shadow-md" 
+                                />
                                 <span className="lg:text-xl font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
                                     {s.label}
                                 </span>
                                 <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
                                     {s.handle}
-                                </span>
-                            </div>
-                        </Card><Card>
-                                <Link
-                                    href="https://www.credly.com/users/yaswanthreddy-varikuti_ai"
-                                    target="_blank"
-                                    className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
-                                >
-                                    <span
-                                        className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
-                                        aria-hidden="true" />
-                                    <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-                                        <Image
-                                            src="/credly page.png"
-                                            alt="Credly Profile"
-                                            width={200} height={100}
-                                            className="rounded-lg shadow-md" />
-                                    </span>{" "}
-                                    <div className="z-10 flex flex-col items-center">
-                                        <span className="lg:text-xl font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
-                                            "Credly Profile"
-                                        </span>
-                                        <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-                                            "Credly Profile"
-                                        </span>
-                                    </div>
-                                </Link>
-                            </Card></>
+                                 </span>
+                             </div>
+                        </Card>
                     ))}
+                    <Card>
+                            <Link
+                                href="https://www.credly.com/users/yaswanthreddy-varikuti_ai"
+                                target="_blank"
+                                className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
+                            >
+                                <span
+                                    className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
+                                    aria-hidden="true" />
+                                <span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+                                    <Image
+                                        src="/credly page.png"
+                                        alt="Credly Profile"
+                                        width={200} height={100}
+                                        className="rounded-lg shadow-md" />
+                                </span>{" "}
+                                <div className="z-10 flex flex-col items-center">
+                                    <span className="lg:text-xl font-medium duration-150 xl:text-2xl text-zinc-200 group-hover:text-white font-display">
+                                        "Credly Profile"
+                                    </span>
+                                    <span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
+                                        "Credly Profile"
+                                    </span>
+                                </div>
+                            </Link>
+                    </Card>
                 </div>
             </div>
         </div>
